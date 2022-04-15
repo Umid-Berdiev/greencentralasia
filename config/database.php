e073e8183041217bc9b1834b32a80e7d55900d5d
Umid <umid-berdiev82@mail.ru>
2022-04-15 14:09:54 +0500

    next changes

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
       'driver'   => 'mysql',
       'host' => env('DB_HOST', '127.0.0.1'),
       'port' => env('DB_PORT', '3306'),
-      'database' => 'tracker',
+      'database' => 'greencen_tracker',
       'username' => env('DB_USERNAME', 'forge'),
       'password' => env('DB_PASSWORD', ''),
       'strict' => false,    // to avoid problems on some MySQL installs
