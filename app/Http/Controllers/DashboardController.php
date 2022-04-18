d4cf71f4c1a414d28dab33e85b81cbb662eebe28
Umid <umid-berdiev82@mail.ru>
2022-04-18 12:59:21 +0500

    next changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,32 +11,29 @@ class DashboardController extends Controller
 {
   public function index(Request $request)
   {
-    $today = new Minutes();
-    $today->setStart(Carbon::today());
-    $today->setEnd(Carbon::now());
+    $today = Carbon::today()->diffInMinutes(Carbon::now());
+    $yesterday = Carbon::yesterday()->diffInMinutes(Carbon::today());
+    $last_week = Carbon::today()->subWeek()->diffInMinutes(Carbon::today());
+    // $last_month = Carbon::today()->subMonth()->diffInMinutes(Carbon::today());
     // dd($today);
 
-    $yesterday = new Minutes();
-    $yesterday->setStart(Carbon::yesterday());
-    $yesterday->setEnd(Carbon::today());
+    // $yesterday = new Minutes();
+    // $yesterday->setStart(Carbon::yesterday());
+    // $yesterday->setEnd(Carbon::today());
     // dd($yesterday);
 
-    $last_week = new Minutes();
-    $last_week->setStart(Carbon::today()->subDays(7));
-    $last_week->setEnd(Carbon::today());
+    // $last_week = new Minutes();
+    // $last_week->setStart(Carbon::today()->subDays(7));
+    // $last_week->setEnd(Carbon::today());
     // dd($last_week);
 
     $start_of_last_month = new Carbon('first day of last month');
     $end_of_last_month = new Carbon('last day of last month');
-    $last_month = new Minutes();
-    $last_month->setStart($start_of_last_month);
-    $last_month->setEnd($end_of_last_month);
+    $last_month = $start_of_last_month->diffInMinutes($end_of_last_month);
 
     $start_of_all_time = Carbon::create(2022, 1, 1, 0, 0, 0);
     $end_of_all_time = Carbon::now();
-    $all_time = new Minutes();
-    $all_time->setStart($start_of_all_time);
-    $all_time->setEnd($end_of_all_time);
+    $all_time = $start_of_all_time->diffInMinutes($end_of_all_time);
 
     $online_users = Tracker::onlineUsers()->count(); // defaults to 3 minutes
     $today_users = Tracker::sessions($today)->count();
