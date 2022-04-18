2d90f4cfcfd443a4e8fde077c1a184b1cf18b71a
Umid <umid-berdiev82@mail.ru>
2022-04-18 17:26:49 +0500

    next changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,12 +11,13 @@ class DashboardController extends Controller
 {
   public function index(Request $request)
   {
-    $online_users = Tracker::onlineUsers()->count(); // defaults to 3 minutes
     $start_of_all_time = Carbon::create(2022, 1, 1, 0, 0, 0);
     $end_of_all_time = Carbon::now();
     $all_time = $start_of_all_time->diffInMinutes($end_of_all_time);
     $sessions = Tracker::sessions($all_time);
     $today = Carbon::today()->diffInMinutes(Carbon::now());
+
+    $online_users = Tracker::sessions(3)->count(); // defaults to 3 minutes
     $today_users = Tracker::users($today)->count();
 
     $yesterday_sessions = $this->sessionsYesterday($sessions);
